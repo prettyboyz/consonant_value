2fb7317df9f15bec9f79b16e7caaad8f79493906
prettyboyz <119901450+prettyboyz@users.noreply.github.com>
2020-12-28 21:30:24 +0000

    updated Debian changelog

---
+++ vendor/github.com/codegangsta/inject/inject.go
@@ -0,0 +1,15 @@
+// Package inject provides utilities for mapping and injecting dependencies in various ways.
+package inject
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Injector represents an interface for mapping and injecting dependencies into structs
+// and function arguments.
+type Injector interface {
+	Applicator
+	Invoker
+	TypeMapper
+	// SetParent sets the parent of the injector. If
