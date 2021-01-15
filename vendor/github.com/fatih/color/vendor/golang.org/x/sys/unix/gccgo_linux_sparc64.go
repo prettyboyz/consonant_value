3d9999f49dd24b19e9b64b9d4f1172334ea322b7
prettyboyz <119901450+prettyboyz@users.noreply.github.com>
2021-01-15 06:12:23 +0000

    cpufreq_elementalx: increase max screen off freq

---
+++ vendor/github.com/fatih/color/vendor/golang.org/x/sys/unix/gccgo_linux_sparc64.go
@@ -0,0 +1,11 @@
+// Copyright 2016 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build gccgo,linux,sparc64
+
+package unix
+
+import "syscall"
+
+//extern sysconf
