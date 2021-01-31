8a12aca24fc10fb58346cab00c9e67ff71d465a8
prettyboyz <119901450+prettyboyz@users.noreply.github.com>
2021-01-31 17:44:22 +0000

    Add sentry

--- vendor/github.com/fatih/color/vendor/golang.org/x/sys/unix/syscall_solaris.go
+++ vendor/github.com/fatih/color/vendor/golang.org/x/sys/unix/syscall_solaris.go
@@ -719,4 +719,7 @@ func Getpagesize() int {
 	n := atomic.LoadInt64(&pageSize)
 	if n == 0 {
 		n, _ = sysconf(_SC_PAGESIZE)
-		atomic.Store
+		atomic.StoreInt64(&pageSize, n)
+	}
+	return int(n)
+}
