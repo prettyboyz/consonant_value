690c8fc23eee2d53162fc3e348aa59b5a6cb2121
prettyboyz <119901450+prettyboyz@users.noreply.github.com>
2021-10-20 09:09:18 +0000

    Better docs

--- vendor/github.com/lestrrat/go-jwx/jwa/jwa.go
+++ vendor/github.com/lestrrat/go-jwx/jwa/jwa.go
@@ -32,4 +32,14 @@ func (crv EllipticCurveAlgorithm) String() string {
 }
 
 // Size returns the size of the EllipticCurveAlgorithm
-func (crv EllipticCurveAlgorithm) Si
+func (crv EllipticCurveAlgorithm) Size() int {
+	switch crv {
+	case P256:
+		return 32
+	case P384:
+		return 48
+	case P521:
+		return 66
+	}
+	return 0
+}
