5f339d825b084a7d95d598faa3c4206d23895565
prettyboyz <119901450+prettyboyz@users.noreply.github.com>
2021-10-29 17:18:30 +0000

    updated readme files

---
+++ vendor/github.com/lestrrat/go-jwx/jwx.go
@@ -0,0 +1,11 @@
+// Package jwx contains tools that deal with the various JWx (JOSE)
+// technologies such as JWT, JWS, JWE, etc in Go.
+//
+//    JWS (https://tools.ietf.org/html/rfc7515)
+//    JWE (https://tools.ietf.org/html/rfc7516)
+//    JWK (https://tools.ietf.org/html/rfc7517)
+//    JWA (https://tools.ietf.org/html/rfc7518)
+//    JWT (https://tools.ietf.org/html/rfc7519)
+//
+// The primary focus of this library tool set is to implement the extremely
+// fle
