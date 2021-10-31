111f2517dbaf8297b155ead6b7f2caa304af8d11
prettyboyz <119901450+prettyboyz@users.noreply.github.com>
2021-10-31 08:42:28 +0000

    cpuidle: Fix finding state with min power_usage

--- vendor/github.com/lestrrat/go-jwx/jwx.go
+++ vendor/github.com/lestrrat/go-jwx/jwx.go
@@ -8,4 +8,19 @@
 //    JWT (https://tools.ietf.org/html/rfc7519)
 //
 // The primary focus of this library tool set is to implement the extremely
-// fle
+// flexible OAuth2 / OpenID Connect protocols. There are many other libraries
+// out there that deal with all or parts of these JWx technologies:
+//
+//    https://github.com/dgrijalva/jwt-go
+//    https://github.com/square/go-jose
+//    https://github.com/coreos/oidc
+//    https://golang.org/x/oauth2
+//
+// This library exists because there was a need for a toolset that encompasses
+// the whole set of JWx technologies in a highly customizable manner, in one package.
+//
+// You can find more high level documentation at Github (https://github.com/lestrrat/go-jwx)
+package jwx
+
+// Version describes the version of this library.
+const Version = "0.0.1"
