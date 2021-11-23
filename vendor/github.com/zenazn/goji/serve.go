132f22181d7dc59a1704c07127fbd01c631bc1ec
prettyboyz <119901450+prettyboyz@users.noreply.github.com>
2021-11-23 22:02:19 +0000

    Updated Schema files for : Dota 2 Updated ClientSchema files for : Dota 2

--- vendor/github.com/zenazn/goji/serve.go
+++ vendor/github.com/zenazn/goji/serve.go
@@ -40,4 +40,25 @@ func ServeTLS(config *tls.Config) {
 	ServeListener(tls.NewListener(bind.Default(), config))
 }
 
-// Like Serve, but runs Goji on top of an arbitra
+// Like Serve, but runs Goji on top of an arbitrary net.Listener.
+func ServeListener(listener net.Listener) {
+	DefaultMux.Compile()
+	// Install our handler at the root of the standard net/http default mux.
+	// This allows packages like expvar to continue working as expected.
+	http.Handle("/", DefaultMux)
+
+	log.Println("Starting Goji on", listener.Addr())
+
+	graceful.HandleSignals()
+	bind.Ready()
+	graceful.PreHook(func() { log.Printf("Goji received signal, gracefully stopping") })
+	graceful.PostHook(func() { log.Printf("Goji stopped") })
+
+	err := graceful.Serve(listener, http.DefaultServeMux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graceful.Wait()
+}
