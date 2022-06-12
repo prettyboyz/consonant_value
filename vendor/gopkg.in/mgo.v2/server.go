cc137366768e586ef7b88e918581fe9e66581c77
prettyboyz <119901450+prettyboyz@users.noreply.github.com>
2022-06-12 10:43:43 +0000

    Added pictures

--- vendor/gopkg.in/mgo.v2/server.go
+++ vendor/gopkg.in/mgo.v2/server.go
@@ -26,66 +26,31 @@
 
 package mgo
 
-type queue struct {
-	elems               []interface{}
-	nelems, popi, pushi int
-}
+import (
+	"errors"
+	"net"
+	"sort"
+	"sync"
+	"time"
 
-func (q *queue) Len() int {
-	return q.nelems
-}
+	"gopkg.in/mgo.v2/bson"
+)
 
-func (q *queue) Push(elem interface{}) {
-	//debugf("Pushing(pushi=%d popi=%d cap=%d): %#v\n",
-	//       q.pushi, q.popi, len(q.elems), elem)
-	if q.nelems == len(q.elems) {
-		q.expand()
-	}
-	q.elems[q.pushi] = elem
-	q.nelems++
-	q.pushi = (q.pushi + 1) % len(q.elems)
-	//debugf(" Pushed(pushi=%d popi=%d cap=%d): %#v\n",
-	//       q.pushi, q.popi, len(q.elems), elem)
-}
+// ---------------------------------------------------------------------------
+// Mongo server encapsulation.
 
-func (q *queue) Pop() (elem interface{}) {
-	//debugf("Popping(pushi=%d popi=%d cap=%d)\n",
-	//       q.pushi, q.popi, len(q.elems))
-	if q.nelems == 0 {
-		return nil
-	}
-	elem = q.elems[q.popi]
-	q.elems[q.popi] = nil // Help GC.
-	q.nelems--
-	q.popi = (q.popi + 1) % len(q.elems)
-	//debugf(" Popped(pushi=%d popi=%d cap=%d): %#v\n",
-	//       q.pushi, q.popi, len(q.elems), elem)
-	return elem
-}
-
-func (q *queue) expand() {
-	curcap := len(q.elems)
-	var newcap int
-	if curcap == 0 {
-		newcap = 8
-	} else if curcap < 1024 {
-		newcap = curcap * 2
-	} else {
-		newcap = curcap + (curcap / 4)
-	}
-	elems := make([]interface{}, newcap)
-
-	if q.popi == 0 {
-		copy(elems, q.elems)
-		q.pushi = curcap
-	} else {
-		newpopi := newcap - (curcap - q.popi)
-		copy(elems, q.elems[:q.popi])
-		copy(elems[newpopi:], q.elems[q.popi:])
-		q.popi = newpopi
-	}
-	for i := range q.elems {
-		q.elems[i] = nil // Help GC.
-	}
-	q.elems = elems
-}
+type mongoServer struct {
+	sync.RWMutex
+	Addr          string
+	ResolvedAddr  string
+	tcpaddr       *net.TCPAddr
+	unusedSockets []*mongoSocket
+	liveSockets   []*mongoSocket
+	closed        bool
+	abended       bool
+	sync          chan bool
+	dial          dialer
+	pingValue     time.Duration
+	pingIndex     int
+	pingCount     uint32
+	pingWindow    [6]time.Durat
